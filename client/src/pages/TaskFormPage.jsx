5d7b1874ceef324746597dd260a707de3d78615f
alexisdev <alexisescobardev@gmail.com>
2024-04-24 19:21:50 -0300

    add put

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,18 +1,47 @@
 import { useForm } from 'react-hook-form'
-import { createTask, deleteTask } from '../api/task.api'
+import { createTask, deleteTask, getTask, updateTask } from '../api/task.api'
 import { useNavigate, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export function TaskFormPage () {
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue
+  } = useForm()
 
   const navigate = useNavigate()
   const params = useParams()
-  console.log(params)
 
   const handleTaskSubmit = handleSubmit(async (data) => {
-    await createTask(data)
+    if (params.id) {
+      await updateTask(params.id, data)
+    } else {
+      await createTask(data)
+    }
+
     navigate('/')
   })
+
+  useEffect(() => {
+    const loadTask = async () => {
+      if (params.id) {
+        const {
+          data: {
+            title,
+            descripcion,
+            finish
+          }
+        } = await getTask(params.id)
+        setValue('title', title)
+        setValue('descripcion', descripcion)
+        setValue('finish', finish)
+      }
+    }
+
+    loadTask()
+  }, [])
 
   return (
     <div className='h-[80vh] py-4 flex flex-col items-center'>
@@ -45,7 +74,19 @@ export function TaskFormPage () {
         <button className='border border-blue-400 w-[300px] m-auto py-2 rounded-full bg-teal-500 text-white font-semibold text-xl hover:bg-teal-200 hover:text-gray-500 transition-all'>Guardar</button>
       </form>
 
-      {params.id && <button className='border bg-red-600 text-white px-4 py-2 mt-4 rounded-full w-28 hover:bg-red-400 transition-all'>Delete</button>}
+      {params.id &&
+        <button
+          className='border bg-red-600 text-white px-4 py-2 mt-4 rounded-full w-28 hover:bg-red-400 transition-all'
+          onClick={async () => {
+            const accepted = window.confirm('Estas seguro?')
+            if (accepted) {
+              await deleteTask(params.id)
+              navigate('/')
+            }
+          }}
+        >
+          Delete
+        </button>}
     </div>
   )
 }
