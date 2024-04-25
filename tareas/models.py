97c982f94f6f7538e6142f0ec8a2c4c22b66fa07
alexisdev <alexisescobardev@gmail.com>
2024-04-24 22:59:27 -0300

    update model todo

--- tareas/models.py
+++ tareas/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # Create your models here.
 class Tareas(models.Model):
   title = models.CharField(max_length=1000)
-  descripcion = models.TextField(blank=True)
   finish = models.BooleanField(default=False)
 
   def __str__(self) -> str:
-    return f'title: {self.title}, descripcion: {self.descripcion}, finish: {self.finish}'
+    return f'title: {self.title}, finish: {self.finish}'
