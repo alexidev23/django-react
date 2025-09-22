f66b3f23e75273b158948cad9c300349a3f9f6e9
alexisDev23 <alexisescobar1234@gmail.com>
2025-09-22 15:56:11 -0300

    update project

--- tareas/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Tareas(models.Model):
-  title = models.CharField(max_length=1000)
-  finish = models.BooleanField(default=False)
-
-  def __str__(self) -> str:
-    return f'title: {self.title}, finish: {self.finish}'
--- tareas/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .serializer import TareasSerializer
-from .models import Tareas
-
-# Create your views here.
-class view(viewsets.ModelViewSet):
-  serializer_class = TareasSerializer
-  queryset = Tareas.objects.all()
